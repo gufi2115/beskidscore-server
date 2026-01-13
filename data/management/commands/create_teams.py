088293052f3da9ebbe0abb0155cbe807527a251c
gufi <buber2006xdd@gmail.com>
2026-01-13 18:01:27 +0100

    new commit

--- data/management/commands/create_teams.py
+++ data/management/commands/create_teams.py
@@ -6,7 +6,7 @@ import os
 class Command(BaseCommand):
     def handle(self, *args, **options):
         current_dir = os.path.dirname(__file__)
-        json_path = os.path.join(current_dir, 'matches/Klasa_okręgowa_2025_2026.json')
+        json_path = os.path.join(current_dir, 'matches/Klasa_B_2025_2026.json')
 
         with open(json_path,'r', encoding='UTF-8') as json_file:
             json_data = json.load(json_file)
@@ -14,3 +14,6 @@ class Command(BaseCommand):
         if json_data:
             teams_from_json = json_data[-1]['teams']
             teams_creator(teams_from_json)
+
+        with open(json_path,'w', encoding='UTF-8') as json_file:
+            json.dump(json_data[:-1], json_file,ensure_ascii=False, indent=4)
